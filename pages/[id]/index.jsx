51375bab42001d40b91d3c8c3140c7981e5926c0
Hauke <hank.la.boon@gmail.com>
2024-12-18 11:23:21 +0100

    add basic styling to GameDetails

--- pages/[id]/index.jsx
+++ pages/[id]/index.jsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { Fragment } from "react";
 import BackButton from "@/components/BackButton";
+import styled from "styled-components";
 
 export default function GameDetails({ initialGames }) {
   const router = useRouter();
@@ -8,12 +9,51 @@ export default function GameDetails({ initialGames }) {
 
   const selectedGame = initialGames.find((game) => game.id === id);
 
+  if (!selectedGame) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <Fragment>
+    <>
       <BackButton href={"/"}></BackButton>
-      <p>{selectedGame.title}</p>
-      <p>{selectedGame.rating}</p>
-      <p>{selectedGame.description}</p>
-    </Fragment>
+      <StyledArticle>
+        <StyledRowWrapper>
+          <StyledTitle>{selectedGame.title}</StyledTitle>
+          <StyledRating>Rating: {selectedGame.rating}</StyledRating>
+        </StyledRowWrapper>
+        <StyledDescription>{selectedGame.description}</StyledDescription>
+      </StyledArticle>
+    </>
   );
 }
+
+const StyledArticle = styled.article`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  padding: 10px;
+  p {
+    border: solid 0.2rem black;
+    border-radius: 5px;
+    padding: 5px;
+  }
+`;
+
+const StyledRowWrapper = styled.div`
+  display: flex;
+  gap: inherit;
+  justify-content: space-between;
+`;
+
+const StyledTitle = styled.p`
+  align-content: center;
+`;
+
+const StyledRating = styled.p`
+  align-content: center;
+`;
+
+const StyledDescription = styled.p`
+  flex-basis: 100%;
+`;
