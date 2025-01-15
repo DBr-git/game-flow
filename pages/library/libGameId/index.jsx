9834e627333f8591318b0c649903e8576ef52a7e
Hauke <hank.la.boon@gmail.com>
2025-01-15 11:11:35 +0100

    changed styling

--- pages/library/libGameId/index.jsx
+++ pages/library/libGameId/index.jsx
@@ -1,32 +1,25 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import styled from "styled-components";
 import BackButton from "@/components/buttons/BackButton";
 
 export default function LibraryGameDetails() {
-  const router = useRouter();
+  // const router = useRouter();
   return (
     <>
-      <StyledDiv>
-        <BackButton href="/" />
-        <h1>Name of the Game</h1>
-      </StyledDiv>
+      <StyledContainer>
+        <StyledBackground></StyledBackground>
+        <StyledContent>
+          <BackButton href="/libray/" />
+          <h1>Name of the Game</h1>
+        </StyledContent>
+      </StyledContainer>
+
       <StyledArticle>
         <StyledDescription>{"sdsdsdad"}</StyledDescription>
       </StyledArticle>
     </>
   );
 }
-
-const StyledDiv = styled.div`
-  height: 150px;
-  margin: -0.5em -1.75em 0 -1.75em;
-  display: flex;
-  flex-direction: column;
-  padding: 0.5em 1.75em;
-  background-image: url("/the-witcher-artwork.jpg");
-  background-size: cover;
-  background-position: center;
-`;
 
 const StyledDescription = styled.p`
   flex-basis: 100%;
@@ -36,15 +29,47 @@ const StyledDescription = styled.p`
 const StyledArticle = styled.article`
   display: flex;
   flex-direction: column;
-  flex-wrap: wrap;
   gap: 1rem;
   padding: var(--mainContentPadding);
-  overflow-wrap: anywhere;
   color: var(--headingColor);
   p {
     background-color: var(--backgroundSubSection);
     padding: 0.5em 1em;
     border-radius: var(--borderRadius);
     box-shadow: var(--boxShadow);
+    z-index: 1;
   }
+`;
+
+const StyledContainer = styled.div`
+  position: relative;
+  height: 150px;
+  margin: -0.5em -1.75em 0 -1.75em;
+  padding: 0.5em 1.75em;
+`;
+
+const StyledBackground = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url("/the-witcher-artwork.jpg");
+  background-size: cover;
+  background-position: center;
+  mask: linear-gradient(
+    330deg,
+    var(--primaryBackground),
+    20%,
+    rgba(255, 255, 255, 0)
+  );
+  z-index: 1;
+`;
+
+const StyledContent = styled.div`
+  position: relative;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  z-index: 2;
 `;
