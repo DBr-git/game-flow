7978c00792f0c0e8ee62586fba6ec9c63987df19
Hauke <hank.la.boon@gmail.com>
2025-01-20 11:26:54 +0100

    remove comments

--- utils/sortingGames.js
+++ utils/sortingGames.js
@@ -1,37 +1,17 @@
 export function sortedGamesByAlphabet(filteredGames, order) {
-  const sortedGames = filteredGames.toSorted(
-    (a, b) =>
-      order === "descending" && order !== ""
-        ? b.name.localeCompare(a.name)
-        : a.name.localeCompare(b.name)
-    // {
-    //   if (order === "descending") {
-    //     return b.name.localeCompare(a.name);
-    //   } else if (order === "ascending") {
-    //     return a.name.localeCompare(b.name);
-    //   } else {
-    //     return null;
-    //   }
-    // }
+  const sortedGames = filteredGames.toSorted((a, b) =>
+    order === "descending" && order !== ""
+      ? b.name.localeCompare(a.name)
+      : a.name.localeCompare(b.name)
   );
   return sortedGames;
 }
 
 export function sortedGamesByRating(filteredGames, order) {
-  const sortedGames = filteredGames.toSorted(
-    (a, b) =>
-      order === "descending" && order !== ""
-        ? Number(b.rating) - Number(a.rating)
-        : Number(a.rating) - Number(b.rating)
-    // {
-    //   if (order === "descending") {
-    //     return Number(b.rating) - Number(a.rating);
-    //   } else if (order === "ascending") {
-    //     return Number(a.rating) - Number(b.rating);
-    //   } else {
-    //     return null;
-    //   }
-    // }
+  const sortedGames = filteredGames.toSorted((a, b) =>
+    order === "descending" && order !== ""
+      ? Number(b.rating) - Number(a.rating)
+      : Number(a.rating) - Number(b.rating)
   );
   return sortedGames;
 }
